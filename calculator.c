2a9f36c1e8cb40b00457a4b8cf4f27ca6ac9b1b0
toyer <bobbkrist@gmail.com>
2016-10-24 02:18:23 +0300

    proc write functionality

--- calculator.c
+++ calculator.c
@@ -22,10 +22,43 @@ MODULE_VERSION("1");
 
 static struct proc_dir_entry *first_proc_file, *second_proc_file, *operand_proc_file;
 static const int first_proc_index = 1, second_proc_index = 2, operand_proc_index = 3;
+static char first_buffer[PROC_MAX_SIZE], second_buffer[PROC_MAX_SIZE], operand_buffer[PROC_MAX_SIZE];
+static unsigned long first_buffer_size = 0, second_buffer_size = 0, operand_buffer_size = 0;
 
 static ssize_t proc_write(struct file *filp, const char __user *buffer, size_t count, loff_t *data)
 {
+	char *proc_buffer;
+	unsigned long* buffer_size;
+	const unsigned char *name = filp->f_path.dentry->d_name.name;
+
+	printk(KERN_INFO "proc_write called\n");
+
+
+	if (strcmp(name, PROC_FIRST) == 0) {
+		printk(KERN_INFO "proc_write for first\n");
+		proc_buffer = first_buffer;
+		buffer_size = &first_buffer_size;
+	} else if (strcmp(name, PROC_SECOND) == 0) {
+		printk(KERN_INFO "proc_write for second\n");
+		proc_buffer = second_buffer;
+		buffer_size = &second_buffer_size;
+	} else if (strcmp(name, PROC_OPERAND) == 0) {
+		printk(KERN_INFO "proc_write for operand\n");
+		proc_buffer = operand_buffer;
+		buffer_size = &operand_buffer_size;
+	} else {
+		return 0;
+	}
+
+	*buffer_size = count;
+	if (*buffer_size > PROC_MAX_SIZE ) {
+		*buffer_size = PROC_MAX_SIZE;
+	}
+	if ( copy_from_user(proc_buffer, buffer, *buffer_size) ) {
+		return -EFAULT;
+	}
 	
+	return *buffer_size;
 }
 
 static ssize_t dev_read(struct file * file, char * buf, size_t count, loff_t *ppos)
